d50d2fc3744b3219c9152f34223cd8570f21a338
21giulio21 <21giulio21@gmail.com>
2017-07-21 14:06:10 +0200

    Cambiamenti fatti con ios

--- IosVersion/App Daniel Levy/CercaController.swift
+++ IosVersion/App Daniel Levy/CercaController.swift
@@ -8,12 +8,58 @@
 
 import UIKit
 
-class CercaController: UIViewController {
+class CercaController: UIViewController,UITableViewDataSource,UITableViewDelegate,UISearchBarDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    let cellSearch = UITableViewCell()
+    var news = ["Giulio", "Matteo", "marco"]
+    
+    // tolgo la status bar
+    override var prefersStatusBarHidden: Bool {
+        return false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+        self.SearchBarSetup()
+    }
+    
+    func SearchBarSetup()
+    {
+        let searchBar = UISearchBar(frame: CGRect(x: 0, y: 0, width: (UIScreen.main.bounds.width), height: 50))
+        searchBar.showsScopeBar = true
+        
+        // imposto la search bar
+        searchBar.placeholder = "Cerca stai cercando ?"
+        searchBar.tintColor = UIColor.white
+        // prendo la textfield inserita li dentro per cambiare il colore
+        let textFieldInsideSearchBar = searchBar.value(forKey: "searchField") as? UITextField
+        textFieldInsideSearchBar?.textColor = UIColor.white
+        
+        // imposto lo stile
+        searchBar.searchBarStyle = .minimal
+        //searchBar.scopeButtonTitles = ["Name","Years","By"]
+        
+        searchBar.delegate = self
+        
+        // permetto di eliminare l'auto riposizionamento
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        cellSearch.addSubview(searchBar)
+        
+        // x,y,height,width
+        //searchBar.leftAnchor.constraint(equalTo: topView.leftAnchor).isActive = true
+        //searchBar.bottomAnchor.constraint(equalTo: topView.bottomAnchor).isActive = true
+        //searchBar.centerYAnchor.constraint(equalTo: topView.centerYAnchor).isActive = true
+        //searchBar.widthAnchor.constraint(equalToConstant: topView.bounds.width).isActive = true
+        //searchBar.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        // se voletto metterlo come cella che va su o giu
+        //self.tableView.tableHeaderView = searchBar
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +67,68 @@ class CercaController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if searchText.isEmpty
+        {
+           
+            news = ["Giulio", "Matteo", "marco"]
+            self.tableView.reloadData()
+            
+        }
+        else{
+            
+            
+            filterTabView(ind: searchBar.selectedScopeButtonIndex, searchText: searchText)
+            self.tableView.reloadData()
+        }
     }
-    */
+ 
+    
+    func filterTabView(ind:Int,searchText:String)
+    {
+        
+        news = news.filter({ (mod) -> Bool in
+            print("Testo da cercare: \(searchText)")
+            
+            if mod.contains(searchText)
+            {
+                return true
+            }else
+            {
+                return false
+            }
+        })
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return news.count + 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if indexPath.row == 0
+        {
+            return cellSearch
+        }else
+        {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cella")
+        cell?.textLabel?.text = news[indexPath.row - 1]
+
+        return cell!
+        }
+        
+        
+        
+        
+        
+    }
+    
+    
+
+ 
 
 }
--- IosVersion/App Daniel Levy/MainViewController.swift
+++ IosVersion/App Daniel Levy/MainViewController.swift
@@ -8,24 +8,66 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate{
+    
+    @available(iOS 2.0, *)
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    @IBOutlet weak var tipoAiutoPicker: UIPickerView!
+    @IBOutlet weak var provinciaPicker: UIPickerView!
+    // Array contentente gli anni che devono essere mostrati a video
+    var Num = [String]()
     
     override func viewWillAppear(_ animated: Bool) {
+        //self.navigationController?.navigationBar.isHidden = true
         perform(#selector(controlloLogin), with: nil, afterDelay: 0)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        //self.navigationController?.navigationBar.isHidden = false
+
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        riempimentoPicker()
+        
+        tipoAiutoPicker.delegate = self
+        tipoAiutoPicker.dataSource = self
+        
+        provinciaPicker.delegate = self
+        provinciaPicker.dataSource = self
         
         
         
+    }
+    
+    //  Funzione che viene chiamata quando premo il pulsante cerca
+    //  una volta che ho selezionato la categoria e
+    //
+    @IBAction func cercaButtonPress(_ sender: Any) {
+        let cercaController = self.storyboard?.instantiateViewController(withIdentifier: "CercaController")
+        self.navigationController?.pushViewController(cercaController!, animated: true)
         
+    }
+    
+    func riempimentoPicker()
+    {
+        
+        for i in stride(from: 1930, through: 2018, by: 1)
+        {
+            Num.append("\(i)")
+        }
+        Num.sort { (a, b) -> Bool in
+            a > b
+        }
+    
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+   
+    
     
     func controlloLogin()
     {
@@ -43,6 +85,30 @@ class MainViewController: UIViewController {
         }
     
         
+    }
+    
+    //  Speficico che ci vede essere una sola sezione nel UIPickerView
+    //
+    //
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    //  Specifico che devono esserci tanti elementi nel UIPickerView
+    //  quanti presenti nell'array Num
+    //
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
+    {
+        return Num.count
+    }
+    
+    //  Specifico quali devono essere gli elementi nel UIPickerView
+    //
+    //
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return Num[row]
     }
 
 
